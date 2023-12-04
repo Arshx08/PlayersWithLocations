e69be40a43a29ee3845e1ffda3d3b491ddeb103d
Arshdeep Singh <sing7434@sheridancollege.ca>
2023-12-04 13:13:13 -0500

    Initial Commit

---
+++ FinalPracticeTest/FinalPracticeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalPracticeTestApp.swift
+//  FinalPracticeTest
+//
+//  Created by Arshdeep Singh on 2023-12-04.
+//
+
+import SwiftUI
+
+@main
+struct FinalPracticeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
